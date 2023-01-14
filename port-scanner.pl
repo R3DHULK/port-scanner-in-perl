605c34a046bb1becc401f0e3a36210ac15704550
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2023-01-14 21:10:19 +0530

    Port Scanner In Perl

--- port-scanner.pl
+++ port-scanner.pl
@@ -3,9 +3,9 @@ use strict;
 use warnings;
 use IO::Socket;
 
-my $host = shift;
-my $start_port = shift;
-my $end_port = shift;
+my $host = $ARGV[0];
+my $start_port = $ARGV[1];
+my $end_port = $ARGV[2];
 
 for my $port ($start_port..$end_port) {
     my $socket = IO::Socket::INET->new(
@@ -18,5 +18,7 @@ for my $port ($start_port..$end_port) {
     if ($socket) {
         print "Port $port is open\n";
         close($socket);
+    }else{
+        print "Port $port is closed\n";
     }
 }
